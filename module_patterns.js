7fba838fa3a46f198b79ddaa35093195c88f3362
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-08-14 15:42:44 +0530

    Module pattern and revealing module pattern

--- module_patterns.js
+++ module_patterns.js
@@ -1,9 +1,50 @@
 //Basic structure
 
-(function() {
-  //Declare private variables and functions 
+// (function() {
+//   //Declare private variables and functions 
+
+//   return {
+//     //Declare public vars and functions
+//   }
+// })();
+
+//STANDARD MODULE PATTERN
+const UICtrl = (() => {
+  let text = 'Hello World';
+
+  const changeText = () => {
+    const element = document.querySelector('h1');
+    element.textContent = text;
+  }
 
   return {
-    //Declare public vars and functions
+    callChangeText() {
+      changeText();
+      console.log(text);
+    }
   }
 })();
+
+UICtrl.callChangeText();
+
+// REVEALING MODULE PATTERN
+const ItemCtrl = (() => {
+  let data = [];
+
+  function add(item) {
+    data.push(item);
+    console.log('Item added....');
+  }
+
+  function get(id) {
+    return data.find(item => item.id === id)
+  }
+
+  return {
+    add,
+    get
+  }
+})();
+
+ItemCtrl.add({id: 1, name: 'John'});
+console.log(ItemCtrl.get(1));
