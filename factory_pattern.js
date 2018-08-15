9b641b77cb1c5391c6f1197c9fdd364c044b2ed2
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-08-15 13:07:35 +0530

    Factory Pattern

---
+++ factory_pattern.js
@@ -0,0 +1,47 @@
+function MemberFactory() {
+  this.createMember = (name, type) => {
+    let member;
+
+    if (type === 'simple') {
+      member = new SimpleMembership(name);
+    } else if (type === 'standard') {
+      member = new StandardMembership(name);
+    } else if (type === 'super') {
+      member = new SuperMembership(name);
+    }
+
+    member.type = type;
+    member.define = function() {
+      console.log(`${this.name} (${this.type}): ${this.cost}`);
+    };
+
+    return member;
+  };
+}
+
+const SimpleMembership = function(name) {
+  //arrow functions do not supprt 'this' keyword
+  this.name = name;
+  this.cost = '$5';
+};
+
+const StandardMembership = function(name) {
+  this.name = name;
+  this.cost = '$15';
+};
+
+const SuperMembership = function(name) {
+  this.name = name;
+  this.cost = '$25';
+};
+
+const members = [];
+const factory = new MemberFactory();
+
+members.push(factory.createMember('John Doe', 'simple'));
+members.push(factory.createMember('Kaladin Stormblessed', 'standard'));
+members.push(factory.createMember('Chris Jackson', 'super'));
+
+members.forEach(member => {
+  member.define();
+});
